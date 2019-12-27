563083c47b1824275a818905e83fb3ccd8ae2fbd
Dan Manheim <manheim@gmail.com>
2019-12-26 22:34:27 -0800

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,7 +21,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "https://enformed.io/1um71wa6/",
+                url: "https://www.enformed.io/1um71wa6/",
                 method: "POST",
                 data: {
                     name: name,
