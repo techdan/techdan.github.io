652514cf8bbfb7bea4a5da54d89933305afc1221
Dan Manheim <manheim@gmail.com>
2019-12-26 22:54:35 -0800

    Update contact_me.js
    Bots + lazy hack

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,6 +20,8 @@ $(function() {
             if (firstName.indexOf(' ') >= 0) {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
+		// kill bots?
+		if ($("input#honeypot").val() == 'no') {
             $.ajax({
                 url: "https://www.enformed.io/1um71wa6/",
                 method: "POST",
@@ -49,15 +51,28 @@ $(function() {
                 error: function(err) {
                     // Fail message
 					console.log(err);
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    //$('#success').html("<div class='alert alert-danger'>");
+                    //$('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                    //    .append("</button>");
+                    //$('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    //$('#success > .alert-danger').append('</div>');
+			
+			// Awful hack - silently fail
+			// Enable button & show success message
+                    $("#btnSubmit").attr("disabled", false);
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
-                    $('#success > .alert-danger').append('</div>');
+                    $('#success > .alert-success')
+                        .append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success')
+                        .append('</div>');
+			
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
             });
+		};
         },
         filter: function() {
             return $(this).is(":visible");
